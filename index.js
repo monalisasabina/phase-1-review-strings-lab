8b183bb9fb6ac0df206f39bc4b7658872d6ca94a
Monalisa Onyango <+>
2024-06-24 14:35:29 +0300

    string review

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+
+const currentUser = 'Monalisa';
+
+//const welcomeMessage = 'Welcome to Flatbook, ';
+
+//const welcomeMessage = currentUser;
+
+//const welcomeMessage = 'Welcome to Flatbook, currentUser';
+
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+//const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+//const shortGreeting='Welcome, '
+
+//const shortGreeting = `Welcome, ${currentUser}`;  //lacks M at the beginning of my name
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
